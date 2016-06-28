a7ad9e91a28311a457f377fc054f18bc85f12c70
Jorge <jogonzal@microsoft.com>
2016-06-28 12:29:28 -0700

    Check in whole test

---
+++ HapParserSample.UnitTests/HtmlParseUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Web;
+using FluentAssertions;
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HapParserSample.UnitTests
+{
+	[TestClass]
+	public class HtmlParseUnitTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var fileContent = File.ReadAllText("myFile.html");
+			var doc = HtmlParse.Parse(fileContent);
+			HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("/html[1]/body[1]/img[1]");
+			nodes.Should().HaveCount(1);
+			nodes[0].Attributes.Should().HaveCount(1);
+			var attribute = nodes[0].Attributes[0];
+			attribute.Name.Should().Be("alt");
+			string htmlDecodedValue = HttpUtility.HtmlDecode(attribute.Value);
+			htmlDecodedValue.Should().Be("&");
+			attribute.Value.Should().Be("&");
+		}
+	}
+}
---
+++ HapParserSample/HtmlParse.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+
+namespace HapParserSample
+{
+	public class HtmlParse
+	{
+		public static HtmlDocument Parse(string html)
+		{
+			var doc = new HtmlDocument()
+			{
+			};
+			doc.LoadHtml(html);
+			return doc;
+		}
+	}
+}
